02882cabd4807a8370fd02a63ec48e9a21fb08ef
lostedmemarbles <ashleyemarble@gmail.com>
2026-01-06 18:24:19 -0600

    DONE

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 'use client'
 import Image from "next/image";
+import Form from 'next/form'
 import { getJSONFromFile, saveShoppingList } from './_data-handling/json-handling';
 import ListItem from './_ui/list-item';
 import { useState } from 'react';
 
 export default function Home() {
+ var itemName = '';
+ var quantity = 1;
+ var unitType = '';
+  function setItemName(val: string) {
+    itemName = val;
+  }
+  function setQuantity(val: number) {
+    quantity = val;
+  }
+  function setUnitType(val: string) {
+    unitType = val;
+  }
   const [shoppingList, updateList] = useState(getJSONFromFile());
+  const [addingItem, setAddingItem] = useState(false);
   shoppingList.items = sortItems( shoppingList.items);
   const [orderedList, refreshList] = useState(createListItems(shoppingList, onFaveChange));
 function onFaveChange(itemId: string, isFave: boolean) {
@@ -17,8 +31,26 @@ function onFaveChange(itemId: string, isFave: boolean) {
   updateList(shoppingList);
   refreshList(createListItems(shoppingList, onFaveChange));
 }
-function handleNewItem() {
-
+function handleNewItem(e: React.FormEvent<HTMLFormElement>) {
+  e.preventDefault();
+  var itemId = 'item-'+shoppingList.items.length+1;
+  var newItem = {      "id": itemId,
+      "name": itemName,
+      "quantity": quantity,
+      "unit": unitType,
+      "isFavorite": 0};
+      console.log('ADDING NEW ITEM');
+  shoppingList.items.push(newItem);
+  shoppingList.items = sortItems(shoppingList.items);
+  updateList(shoppingList);
+  refreshList(createListItems(shoppingList, onFaveChange));
+  setAddingItem(false);
+}
+function createNewItem() {
+  setAddingItem(true);
+  setUnitType('');
+  setQuantity(1);
+  setUnitType('');
 }
   return (
     <div className="flex min-h-screen  justify-center bg-pink-300 font-sans dark:bg-gray-800">
@@ -30,7 +62,17 @@ function handleNewItem() {
           width={100}
           height={20}
           priority
-        /> {!!shoppingList ? (
+        />
+        
+        {addingItem ? (<form onSubmit={handleNewItem} className="flex flex-col  gap-2 color-gray-300 border-black sm:items-start sm:text-left">
+          <label htmlFor='item-name'>Item Name</label><input  type='text' onChange={(e) => setItemName(e.target.value)}
+          required id='item-name'></input>
+          <label htmlFor='item-name'>Quantity</label><input type='number' onChange={(e) => setQuantity(e.target.value)} required></input>
+          <label htmlFor='item-name'>Unit Type</label><input  type='text' onChange={(e) => setUnitType(e.target.value)} required id='item-name'></input>
+          <button type='submit'>Add</button>
+          </form>) : ''}
+
+         {!!shoppingList ? (
         <div className="flex flex-col  gap-2 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold tracking-tight text-black dark:text-pink-200">
             {shoppingList.title}
@@ -45,7 +87,7 @@ function handleNewItem() {
         </div>
         }
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <button onClick={handleNewItem}>New Item</button>
+          <button onClick={createNewItem}>New Item</button>
         </div>
       </main>
     </div>
