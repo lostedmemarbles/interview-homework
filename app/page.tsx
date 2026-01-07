a5c2cf1b969cf8570e175df5da89333fa200522c
lostedmemarbles <ashleyemarble@gmail.com>
2026-01-06 21:50:53 -0600

    cleanup and better React state management

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Form from 'next/form'
 import { getJSONFromFile, saveShoppingList } from './_data-handling/json-handling';
 import ListItem from './_ui/list-item';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export default function Home() {
   const [itemName, setItemName] = useState('');
@@ -12,8 +12,14 @@ export default function Home() {
   const [shoppingList, updateList] = useState(getJSONFromFile());
   const [addingItem, setAddingItem] = useState(false);
   shoppingList.items = sortItems( shoppingList.items);
-  const [orderedList, refreshList] = useState(createListItems(shoppingList, onFaveChange));
+  const orderedItems = useMemo(() => {
+  return sortItems([...shoppingList.items]);
+}, [shoppingList.items]);
+
 function onFaveChange(itemId: string, isFave: boolean) {
+  console.log('ONCHANGE', itemId, isFave);
+  shoppingList.items = sortItems(shoppingList.items);
+  console.log(shoppingList.items);
   updateList(prev => ({
     ...prev,
     items: sortItems(
@@ -28,16 +34,18 @@ function onFaveChange(itemId: string, isFave: boolean) {
 function handleNewItem(e: React.FormEvent<HTMLFormElement>) {
   e.preventDefault();
   var itemId = 'item-'+shoppingList.items.length+1;
-  var newItem = {      "id": itemId,
+  updateList(prev => {
+    const newItem = {
+      "id": itemId,
       "name": itemName,
       "quantity": quantity,
       "unit": unitType,
-      "isFavorite": 0};
-      console.log('ADDING NEW ITEM');
-  shoppingList.items.push(newItem);
-  shoppingList.items = sortItems(shoppingList.items);
-  updateList(shoppingList);
-  refreshList(createListItems(shoppingList, onFaveChange));
+      "isFavorite": 0
+    };
+    return {...prev,
+      items: sortItems([...prev.items, newItem])
+    };
+  });
   setAddingItem(false);
 }
 function createNewItem() {
@@ -72,7 +80,19 @@ function createNewItem() {
             {shoppingList.title}
           </h1>
           <div id='list-items' className="max-w-lg text-lg text-pink-600 dark:text-pink-400">
-            <ol>{orderedList}</ol>
+            <ol>
+              {orderedItems.map(item => (
+                <ListItem
+                  key={item.id}
+                  itemName={item.name}
+                  quantity={item.quantity}
+                  unit={item.unit}
+                  isFave={!!item.isFavorite}
+                  itemId={item.id}
+                  onChangeFave={onFaveChange}
+                />
+              ))}
+  </ol>
           </div>
         </div>) : <div className="flex flex-col  gap-2 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold tracking-tight text-black dark:text-pink-200">
